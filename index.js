4d4a26baf423431af13c3104edb07bb5bb8d314b
habibur-01 <h.r.habibur08@gmail.com>
2024-03-30 06:50:43 +0600

    delete and update data from users

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000
 
 // middleware
 app.use(cors({
-    origin:[
+    origin: [
         'http://localhost:5173'
     ],
     credentials: true
@@ -15,7 +15,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0.cbqlcas.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -41,38 +41,71 @@ async function run() {
 
 
         // post user data
-        app.post("/users", async(req, res) => {
+        app.post("/users", async (req, res) => {
             const user = req.body
             const result = await userCollection.insertOne(user)
             res.send(result)
         })
         // get user data
-        app.get("/users", async(req, res)=>{
+        app.get("/users", async (req, res) => {
             const result = await userCollection.find().toArray()
             res.send(result)
         })
+        // delete user
+        app.delete("/users/:id", async (req, res) => {
+            const id = req.params.id
+            const query = {_id: new ObjectId(id) }
+            const result = await userCollection.deleteOne(query)
+            res.send(result)
+        })
+        // update user data(make admin)
+        app.patch("/users/admin/:id", async(req, res)=>{
+            const id = req.params.id
+            const query = {_id: new ObjectId(id)}
+            const updateDoc={
+                $set:{
+                   role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(query, updateDoc)
+            res.send(result)
+        })
         // get menu data
-        app.get("/menu", async(req, res) => {
+        app.get("/menu", async (req, res) => {
             const result = await menuCollection.find().toArray()
             res.send(result)
         })
 
         // get reviews data
-        app.get("/reviews", async(req, res) => {
+        app.get("/reviews", async (req, res) => {
             const result = await reviewCollection.find().toArray()
             res.send(result)
         })
 
-        app.post("/carts", async(req, res)=>{
-            const cartItem =req.body
+        app.post("/carts", async (req, res) => {
+            const cartItem = req.body
             const result = await cartCollection.insertOne(cartItem)
             res.send(result)
         })
-        app.get("/carts", async(req, res)=>{
-            
-            const result = await cartCollection.find().toArray()
+        app.get("/carts", async (req, res) => {
+            let query = {}
+            const email = req.query.email
+            if (req.query?.email) {
+                query = { email: email }
+            }
+
+            const result = await cartCollection.find(query).toArray()
             res.send(result)
         })
+
+        app.delete("/carts/:id", async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await cartCollection.deleteOne(query)
+            res.send(result)
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
